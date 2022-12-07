391c185f1a27948089293e468573ab39ed274a56
Matheus Silva <msantossilva005@gmail.com>
2022-12-06 23:29:39 -0300

    Código do maps

--- lib/pages/mobile/page_plantation.dart
+++ lib/pages/mobile/page_plantation.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PagePlantation extends StatefulWidget {
   const PagePlantation({super.key});
@@ -9,11 +11,43 @@ class PagePlantation extends StatefulWidget {
 }
 
 class _PagePlantationState extends State<PagePlantation> {
+  final Completer<GoogleMapController> _mapcontroller = Completer();
+
+  static const CameraPosition _center =
+      CameraPosition(target: LatLng(45.521563, -122.677433), zoom: 14);
+
+  final List<Marker> _marker = [];
+  final List<Marker> _branch = const [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(45.521563, -122.677433),
+        infoWindow: InfoWindow(title: 'testando')),
+    Marker(
+      markerId: MarkerId('2'),
+      position: LatLng(45.511100, -122.677433),
+      infoWindow: InfoWindow(title: 'testando 2', snippet: 'testando'),
+    )
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _marker.addAll(_branch);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
-      body: Center(child: Text('PLANTAÇÃO')),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('PLANTAÇÃO'),
+        backgroundColor: Colors.green[700],
+      ),
+      body: GoogleMap(
+          markers: Set<Marker>.of(_marker),
+          onMapCreated: (GoogleMapController controller) {
+            _mapcontroller.complete(controller);
+          },
+          initialCameraPosition: _center),
     );
   }
 }
